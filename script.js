efa31a2286ad5ed2e4e0b87510b9b942f2b8eea5
propg2004 <161467858+propg2004@users.noreply.github.com>
2024-07-03 17:24:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const sname = document.getElementById("name")
 
 
 
-title.innerText = "My Love, My heart, My cutiepie<3";
+title.innerText = "Wishing happiest birthday to my best best best best best Friend";
 
-desc.innerText = "I cannot tell you ho much I love you. These past 6 months have been the best 6 months of my life and you are the reason.I hope I never ever loose you and our bond grows stronger and deeper.";
+desc.innerText = "I hope you find this message well. Mujhe pta hai ye reply dene ki soch rahe ho ki itni mehnat kyu kiye😂. Koi nii mera mann karta rehta hai aapko choti choti surprise dekar khush karne ka😉. Naam ki spelling mein mistak ho sakta hai toh uspe dhayn mat dijeye ga. Emotions ko dekhiye😅.Kaisa lagaa ye surprise??jarurr bataana. And at last I want to give a msg that, Thanks for being in my life, you can't imagine that how much you are important in my life. I hope I never ever loose you and our bond grows stronger and deeper.";
 
-sname.innerText = "Chokrii";
+sname.innerText = "Priyanshu 😊";
